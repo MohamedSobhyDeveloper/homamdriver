850901c27539ad34d386d79938766b6cabec5ae8
Mamdouh <mamdouhsobhy923@gmail.com>
2021-02-22 07:53:07 -0800

    mamdouh_startActivity

--- app/src/main/java/com/otex/homamdriver/view/order/OrderActivity.kt
+++ app/src/main/java/com/otex/homamdriver/view/order/OrderActivity.kt
@@ -3,6 +3,7 @@ package com.otex.homamdriver.view.order
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.otex.homamdriver.R
 import com.otex.homamdriver.databinding.ActivityOrderBinding
---
+++ app/src/main/java/com/otex/homamdriver/view/start/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.otex.homamdriver.view.start
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.otex.homamdriver.R
+import com.otex.homamdriver.databinding.ActivityHomeBinding
+import com.otex.homamdriver.databinding.ActivityMainBinding
+import com.otex.homamdriver.view.home.HomeActivity
+import com.otex.homamdriver.view.order.OrderActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        click()
+    }
+
+    private fun click() {
+        binding.btnStore.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("type", "store")
+            startActivity(intent)
+        }
+
+        binding.btnDriver.setOnClickListener {
+            val intent = Intent(this, OrderActivity::class.java)
+            intent.putExtra("type", "waiting")
+            startActivity(intent)
+        }
+    }
+}
